34e16104734694a2fda07b4f4770be20ff2f2c64
Dimitris Megas <megasd21@yahoo.gr>
2018-11-05 22:19:48 +0200

    NoteDatabase: change val all from auto-conversion back to function

--- app/src/main/java/com/udacity/notepad/data/NoteDatabase.kt
+++ app/src/main/java/com/udacity/notepad/data/NoteDatabase.kt
@@ -19,14 +19,11 @@ class NoteDatabase(context: Context) {
 
     private val helper: NotesOpenHelper = NotesOpenHelper(context)
 
-    val all: List<Note>
-        get() {
-            val cursor = helper.readableDatabase.query(_TABLE_NAME, null, null, null, null, null,
-                    CREATED_AT)
-            val retval = allFromCursor(cursor)
-            cursor.close()
-            return retval
-        }
+    fun getAll(): List<Note> {
+        val cursor = helper.readableDatabase.query(_TABLE_NAME, null, null, null, null, null,
+                CREATED_AT)
+        return cursor.use(this::allFromCursor)
+    }
 
     fun loadAllByIds(vararg ids: Int): List<Note> {
         val questionMarks = StringBuilder()
